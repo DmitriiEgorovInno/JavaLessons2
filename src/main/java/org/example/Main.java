455043c5deddd8bc8e88cf2ec53e9c4d8b2ac4c3
DEgorov <DEgorov@inno.local>
2025-02-23 12:37:16 +0300

    Homework4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,43 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
+
+@ComponentScan
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       ApplicationContext context = new AnnotationConfigApplicationContext(DataSourceConfig.class);
+       UserService userService = context.getBean(UserService.class);
+        //System.out.println("Ищем всех");
+        //userService.getAllUsers().stream().forEach(System.out::println);
+        userService.deleteAllUsers();
+        System.out.println("Добавляем одного пользователя");
+        userService.createUser("Егоров Дмитрий Владимирович");
+        userService.getAllUsers().stream().forEach(System.out::println);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Добавляем несколько пользователей");
+        userService.createUsers(Arrays.asList(
+                "Иванов Иван Иванович",
+                "Петров Петр Петрочик",
+                "Иваной Петр Иванович",
+                "Сидоров Петр Андреевич",
+                "Козлов Александр Сергеевич"));
+        userService.getAllUsers().stream().forEach(System.out::println);
+
+        System.out.println("Ищем по имени");
+        System.out.println(userService.getUserByName("Петров Петр Петрочик"));
+
+        System.out.println("Удаляем по имени");
+        userService.deleteUserByName("Козлов Александр Сергеевич");
+        userService.getAllUsers().stream().forEach(System.out::println);
+
+        System.out.println("Удаляем всех");
+        userService.deleteAllUsers();
+        userService.getAllUsers().stream().forEach(System.out::println);
+
+
     }
 }
